7991b19c68aaa365506c393accc70069525c2c75
sebanputhen <sebastian.puthenveedu@gmail.com>
2026-09-17 14:28:34 +0530

    Initial 1231144

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -22,7 +22,7 @@ import {
   Input,
   Switch,
 } from "antd";
-import signinbg from "../assets/images/img-signin.jpg";
+// import signinbg from "../assets/images/img-signin.jpg";
 import {
   DribbbleOutlined,
   TwitterOutlined,
